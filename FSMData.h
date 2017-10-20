5de81f4420a06234c633d0ef14d16c43344b0cb1
Mario Palmero Pavón <mariopalmero@gmail.com>
2017-10-20 06:59:55 +0200

    Create FSMData.h

---
+++ FSMData.h
@@ -0,0 +1,14 @@
+
+// - Mario Palmero [2017], zlib/libpng licensed.
+
+#pragma once
+
+#include "FSMData.generated.h"
+
+struct FSMData
+{
+	// The most important data to have is the current state
+	// Be careful when inheriting the FSM and the FSMData
+	// E currentState;
+	int currentState;
+};
